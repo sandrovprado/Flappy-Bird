ddd7a6531fb53df61c7fdeb17db4417ed6462a3f
19737 <19737@ALEC>
2022-11-30 00:37:40 -0500

    Add project files.

---
+++ Flappy Bird windows form/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird_windows_form
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 8;
+        int gravity = 1;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+             FlappyBird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            scoreText.Text = "Score: " + score;
+
+            if (pipeBottom.Left < -150)
+            {
+                pipeBottom.Left = 800;
+                score++;
+            }
+            if (pipeTop.Left < -180)
+            {
+                pipeTop.Left = 950;
+                score++;
+            }
+            if (FlappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) || FlappyBird.Bounds.IntersectsWith(pipeTop.Bounds) || FlappyBird.Bounds.IntersectsWith(ground.Bounds) || FlappyBird.Top < -25)
+            {
+                endGame();
+            }
+
+            if(score > 5)
+            {
+                pipeSpeed = 15;
+            }
+
+         
+
+        } 
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+
+            }
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += " *Game Over*";
+
+        }
+    }
+}
